71d9262e754fcbe54f5bbe334bd6ce2aac581906
Anima <miranova.01001101@protonmail.com>
2026-03-19 01:25:48 +0800

    mmm >:(

--- neurobotic-ai/session01.py
+++ neurobotic-ai/session01.py
@@ -5,3 +5,17 @@ arr = np.array([1, 2, 3, 4, 5])
 print(arr)
 
 print(type(arr))
+
+heart_rates = np.array([70, 72, 75, 80])
+
+print(type(heart_rates))
+
+data_a = [1, 2, 3]
+data_b = (1, 2, 3)
+
+arr_a = np.array(data_a)
+arr_b = np.array(data_b)
+
+print(type(arr_a))
+print(type(arr_b))
+
